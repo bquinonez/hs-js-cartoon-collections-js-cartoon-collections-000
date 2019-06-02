7e6a1f69d6093d9b11bf066b110086bc058cd94b
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-06-02 03:39:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function dwarfRollCall(dwarves) {
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+function dwarfRollCall(dwarves){
+ var newDwarves = [];
+ for (var i = 0; i < dwarves.length; i++){
+   newDwarves.push(`${i+1}. ${dwarves[i]} `)
+ }
+ return newDwarves.join("")
 }
+
+
+planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(a=>a.toUpperCase()+"!")
 }
 
-function longPlaneteerCalls(words) {
+
+function longPlaneteerCalls(words){
+  for(var i=0;i<words.length;i++){
+    if (words[i].length>4){
+      return true
+    }
+  }
+  return false
 }
+ 
+var words = ["wind", "fire"]
+longPlaneteerCalls(words)
+ 
 
 function findTheCheese (foods) {
+  for(var i=0;i<foods.length;i++){
+  if (foods[i]=="cheddar"
+  ||foods[i]=="gouda"
+  ||foods[i]=="camembert"
+  ){
+    return foods[i]
+  }
 }
+return "no cheese!"
+}
+ 
